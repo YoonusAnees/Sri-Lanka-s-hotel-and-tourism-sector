bdc0cbaa52674a53bd55ee9f76b563bba27d2f3d
yoonusanees <yoonusaneesniis@gmail.com>
2026-01-09 17:31:37 +0530

    Regression Model Liner + Multi

--- RCommander.R
+++ RCommander.R
@@ -1,11 +1,4 @@
 
-normalityTest(~Revenue, test="ad.test", data=hotels)
-normalityTest(~Revenue, test="shapiro.test", data=hotels)
-normalityTest(~Revenue, test="shapiro.test", data=hotels)
-normalityTest(~Revenue, test="lillie.test", data=hotels)
-normalityTest(~Revenue, test="lillie.test", data=hotels)
-Boxplot( ~ Revenue, data=hotels, id=list(method="y"))
-with(hotels, Dotplot(Revenue, bin=FALSE))
-with(hotels, qqPlot(Revenue, dist="norm", id=list(method="y", n=2, 
-  labels=rownames(hotels))))
+roomsAvailable.Model <- lm(Revenue~RoomsAvailable, data=hotels)
+summary(roomsAvailable.Model)
 
--- v1.R
+++ v1.R
@@ -100,6 +100,73 @@ plot(hotels$Revenue ~ hotels$ADR,
      main = "Revenue vs ADR")
 abline(Revenue_ADR.Model, col = "black")
 
+# -----------------------------
+# Scatter Plot for Revenue with Occupancy Rate
+#-----------------------------
+
+scatterplot(hotels$Revenue~hotels$OccupancyRate, regLine=FALSE, smooth=FALSE,
+            boxplots=FALSE,
+            xlab="Occupancy Rate",
+            ylab="Revenue",
+            main = "Revenue Vs Occupancy Rate",
+            data=hotels)
+
+Revenue_OccupancyRate.Model<-lm(hotels$Revenue~hotels$OccupancyRate,na.exclude=TRUE)
+plot(hotels$Revenue ~ hotels$OccupancyRate,
+     xlab = "Occupancy Rate",
+     ylab = "Revenue",
+     main = "Revenue vs Occupancy Rate")
+abline(Revenue_OccupancyRate.Model, col = "black")
+
+
+Revenue_RoomsAvailable.Model<-lm(hotels$Revenue~hotels$RoomsAvailable,na.exclude=TRUE)
+plot(hotels$Revenue ~ hotels$RoomsAvailable,
+     xlab = "Rooms Available",
+     ylab = "Revenue",
+     main = "Revenue vs Rooms Available")
+abline(Revenue_RoomsAvailable.Model, col = "black")
+
+scatterplotMatrix(
+  ~ADR + OccupancyRate + RoomsAvailable + Revenue,
+  regLine = FALSE,
+  smooth = FALSE,
+  diagonal = list(method = "density"),
+  data = hotels
+)
+
+par(mfrow = c(2, 2))
+plot(Mul.Model)
+
+
+
+
+
+
+
+Revenue_MarketingSpend.Model <- lm(Revenue ~ MarketingSpend, data = hotels, na.action = na.exclude)
+scatterplot(Revenue ~ MarketingSpend, regLine = FALSE, smooth = FALSE,
+            boxplots = FALSE, data = hotels)
+abline(Revenue_MarketingSpend.Model, col = "black")
+
+
+summary(Revenue_MarketingSpend.Model)
+
+
+# Fit the model
+model_marketing <- lm(Revenue ~ MarketingSpend, data = hotels, na.action = na.exclude)
+
+# Residuals vs Fitted plot
+plot(
+  fitted(model_marketing),
+  resid(model_marketing),
+  xlab = "Fitted Revenue",
+  ylab = "Residuals",
+  main = "Residuals vs Fitted Values: Revenue ~ Marketing Spend"
+)
+
+abline(h = 0, col = "green")
+
+
 
 
 
@@ -238,11 +305,103 @@ df %>%
 
 
 
+#-----------------------
+#Regression Model
+#-----------------------
+
+lm(formula = Revenue ~ ADR, data = hotels)
+
+lm(formula = Revenue ~ OccupancyRate, data = hotels)
+
+lm(formula = Revenue ~ RoomsAvailable, data = hotels)
 
 
 
 
 
+
+#--------------------
+#Model Compersion
+#---------------------
+
+plot(hotels$Revenue ~ hotels$ADR,
+     xlab = "ADR",
+     ylab = "Revenue",
+     main = "Revenue vs ADR")
+abline(Revenue_ADR.Model, col = "black")
+
+plot(hotels$Revenue ~ hotels$OccupancyRate,
+     xlab = "OccupancyRate",
+     ylab = "Revenue",
+     main = "Revenue vs OccupancyRate")
+abline(Revenue_OccupancyRate.Model, col = "black")
+
+#---------------
+#Residual 
+#---------------
+
+ADR<-resid(Revenue_ADR.Model)
+plot(fitted(Revenue_ADR.Model),ADR)
+abline(0,0,col="Green")
+
+OR<resid(Revenue_OccupancyRate.Model)
+plot(fitted(Revenue_OccupancyRate.Model),OR)
+abline(0,0,col="Green")
+
+
+
+# fit model safely
+model_occ <- lm(Revenue ~ OccupancyRate, data = hotels)
+roomsAvailable.Model <- lm(Revenue~RoomsAvailable, data=hotels)
+summary(roomsAvailable.Model)
+
+# residual plot
+plot(
+  fitted(model_occ),
+  resid(model_occ),
+  xlab = "Fitted Revenue",
+  ylab = "Residuals",
+  main = "Residuals vs Fitted Values: Revenue ~ Occupancy Rate"
+)
+
+abline(h = 0, col = "green")
+
+# residual plot
+plot(
+  fitted(roomsAvailable.Model),
+  resid(roomsAvailable.Model),
+  xlab = "Fitted Revenue",
+  ylab = "Residuals",
+  main = "Residuals vs Fitted Values: Revenue ~ RoomsAvailable"
+)
+
+abline(h = 0, col = "green")
+
+
+plot(
+  fitted(Revenue_MarketingSpend.Model),
+  resid(Revenue_MarketingSpend.Model),
+  xlab = "Fitted Revenue",
+  ylab = "Residuals",
+  main = "Residuals vs Fitted Values: Revenue ~ RoomsAvailable"
+)
+
+abline(h = 0, col = "green")
+
+
+
+
+---------------
+-  AIC
+--------------
+  
+  # Simple regression models
+AIC(lm(Revenue ~ ADR, data = hotels))
+AIC(lm(Revenue ~ OccupancyRate, data = hotels))
+AIC(lm(Revenue ~ RoomsAvailable, data = hotels))
+
+# Multiple regression model
+AIC(RegModel.9)
 
 
 
